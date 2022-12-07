de6f44523c9fbc96cd4dc978b34455fd60954c67
j1matta <jmatta1980@hotmail.com>
2022-12-07 01:26:42 -0500

    second commit

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 
 
+
+
+
+def print_goodbye():
+    print('goodbuy')
+
+
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
@@ -7,5 +14,6 @@ def print_hi(name):
 
 if __name__ == '__main__':
     print_hi('Joe')
+    print_goodbye()
 
 
