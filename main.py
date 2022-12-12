c5cd8c08d1a3c8d98f9fe57339c41d206a18a8f4
j1matta <j1matta@bridgew.edu>
2022-12-12 02:10:53 -0500

    DEMO Clean Commit

--- main.py
+++ main.py
@@ -4,10 +4,6 @@ def some_other_function():
     print('do stuff')
 
 
-def some_weird_function():
-    print('this is a weird function added by jmatta697')
-
-
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
@@ -21,6 +17,3 @@ if __name__ == '__main__':
     print_hi('Joe')
     some_other_function()
     one_more_function()
-    some_weird_function()
-
-
