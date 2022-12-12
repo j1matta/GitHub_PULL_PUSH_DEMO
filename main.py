00fd07ea99ef4986b26b24e4734139250bbc6a87
j1matta <j1matta@bridgew.edu>
2022-12-12 01:15:07 -0500

    clear funcs

--- main.py
+++ main.py
@@ -4,19 +4,6 @@ def some_other_function():
     print('do stuff')
 
 
-def print_goodbye():
-    print('goodbuy')
-
-
-def fun_function(i):
-    print('you passed ' + 'i')
-
-
-# let's add some stuff
-def another_function():
-    print('just another function')
-
-
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
@@ -28,7 +15,7 @@ def one_more_function():
 
 if __name__ == '__main__':
     print_hi('Joe')
-    print_goodbye()
     some_other_function()
+    one_more_function()
 
 
